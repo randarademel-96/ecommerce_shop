2f04b5037e34bd600c517e34aa7cf25cf8df85cf
Randara <rmgdemel@gmail.com>
2026-07-10 22:49:27 +0530

    Create category

--- src/main/java/lk/ecommerce/shop/controller/CategoryController.java
+++ src/main/java/lk/ecommerce/shop/controller/CategoryController.java
@@ -1,10 +1,29 @@
 package lk.ecommerce.shop.controller;
 
+import jakarta.validation.Valid;
+import lk.ecommerce.shop.controller.request.CategoryRequest;
+import lk.ecommerce.shop.model.Category;
+import lk.ecommerce.shop.service.CategoryService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping(value = "/categories", headers = "x-api-version=v1")
+    public ResponseEntity<Void> create(@Valid @RequestBody CategoryRequest categoryRequest){
+
+        categoryService.create(categoryRequest);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+
+    }
 
 }
