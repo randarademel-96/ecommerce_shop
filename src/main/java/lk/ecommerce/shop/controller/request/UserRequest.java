c63d54fa6e2808ea5bf4cc734974a8059ac38518
Randara <rmgdemel@gmail.com>
2026-07-14 22:29:29 +0530

    User request and response

--- src/main/java/lk/ecommerce/shop/controller/request/UserRequest.java
+++ src/main/java/lk/ecommerce/shop/controller/request/UserRequest.java
@@ -1,4 +1,22 @@
 package lk.ecommerce.shop.controller.request;
 
+import lk.ecommerce.shop.model.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.management.relation.Role;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserRequest {
+
+    private String name;
+    private Gender gender;
+    private Integer phone;
+    private String address;
+    private Role userRole;
 }
