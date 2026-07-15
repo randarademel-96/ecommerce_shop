c108661b69a5b0838ea0974bf96fd3cdb13a67e8
Randara <rmgdemel@gmail.com>
2026-07-15 14:43:42 +0530

    user not found exception

---
+++ src/main/java/lk/ecommerce/shop/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package lk.ecommerce.shop.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
